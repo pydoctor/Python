a7b25159e9cdd6e60452f7e69830874e50e876b4
Maheedhar <maheedharkakarla@gmail.com>
2018-11-04 12:04:23 +0530

    Update 15_assign.py

--- 15_assign.py
+++
@@ -1,8 +0,0 @@
-'''15.	Take two input strings from the user and concatenate 
-two given strings by omitting the first character. '''
-
-
-str1 = input('enter first string: ')
-str2 = input('enter second string: ')
-
-print(str1[1:] + str2[1:])
