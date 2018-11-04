bd272f59cb7e6c334b9407b18debfef4eea06bd8
Maheedhar <maheedharkakarla@gmail.com>
2018-11-04 12:04:18 +0530

    Update 13_assign.py

--- 13_assign.py
+++
@@ -1,7 +0,0 @@
-'''13.	Given a string of even length and print the output as 
-string contains last half added with first half of the given string'''
-
-input_string = 'abcdefghij'
-i = len(input_string)//2
-if len(input_string)%2 == 0:
-    print(input_string[i:] + input_string[0:i])
