bd83b0a2d2c18e6aacbc2514fa07430155e24280
Maheedhar <maheedharkakarla@gmail.com>
2018-11-04 12:04:21 +0530

    Update 14_assign.py

--- 14_assign.py
+++
@@ -1,9 +0,0 @@
-'''14.	WAP replace each string from the given list 
-with the same string which is repeated with length of the string '''
-
-Input = ['a', 'aa', 'abc', 'mahee']
-
-for i in range(0, len(Input)):
-    Input[i] = (Input[i] * len(Input[i]))
-
-print(Input)
