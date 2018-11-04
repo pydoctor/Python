ed829776350d5db2a42c8064b6b6bef1551317bb
Maheedhar <maheedharkakarla@gmail.com>
2018-11-04 12:07:55 +0530

    Update Assignment 1/9_assign.py

---
+++ Assignment 1/9_assign.py
@@ -0,0 +1,18 @@
+
+'''9.	Write a program take input strings of equal length and the length of strings 
+should be greater than 5 and display the output by combining last three characters 
+from the 1st string with first three characters from the second string. 
+'''
+
+str1 = 'INDIAN'
+str2 = 'TIGERS'
+
+print(str1[-3:] + str2[0:3])
+
+
+
+
+
+
+
+
