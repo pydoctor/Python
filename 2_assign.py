56f736d9adfa231538673cc0aa2f10e32adf4778
Maheedhar <maheedharkakarla@gmail.com>
2018-11-04 11:57:29 +0530

    Update 2_assign.py

---
+++ 2_assign.py
@@ -0,0 +1,9 @@
+'''2.	WAP combine all the first characters from given list 
+representing a some of the compelling qualities of a great leader.   
+Qualities = [“Passion”, “Yearning”, “Transparency”, “Humble”, “Open-minded”, “Noble”]'''    
+
+qualities = ['Passion', 'Yearning', 'Transparency', 'Humble', 'Open-minded', 'Noble']
+for i in qualities:
+    print(i[0], end='')
+
+
